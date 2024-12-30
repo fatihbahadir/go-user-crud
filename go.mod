f1505127f782e13e94156917c9454729859e4a69
fatihbahadir <hunkarhyme@gmail.com>
2024-12-30 18:32:46 +0300

    chore: project initialized

---
+++ go.mod
@@ -0,0 +1,3 @@
+module user-crud
+
+go 1.23.4
---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"user-crud/helper"
+)
+
+func main() {
+	fmt.Printf("Server started")
+
+	server := http.Server{
+		Addr: "localhost:8888",
+		Handler: nil,
+	}
+	
+	err := server.ListenAndServe()
+	helper.PanicIfError(err)
+}
